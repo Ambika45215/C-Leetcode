9e72a7f090dc408ecaf29f5bc7d890cfadeaf92e
Ambika45215 <ambikakondamadugula7@gmail.com>
2025-07-25 16:55:27 +0530

    Power of Semicolon

---
+++ Code_5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x = 25;
+    if (x == 30); // Add Semicolon here to see the difference in output and its power  
+    x = 50;
+    if (x == 25)
+        x = x + 1;
+    else
+        x = x - 1;
+    printf("%d", x);
+    return 0;
+}
